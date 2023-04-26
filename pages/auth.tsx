791ad35ff5e37c53124c0d1202c1b8ff4756d899
Mattéo Lo Re <loremattteo@gmail.com>
2023-04-26 18:13:14 +0800

    feat: add email auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,14 @@
+import axios from "axios";
 import { useCallback, useState } from "react";
+import { signIn } from 'next-auth/react';
 
 import Input from "@/components/input";
+import { useRouter } from "next/router";
 
 
 const Auth = () => {
+
+  const router = useRouter();
 
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
@@ -13,7 +18,36 @@ const Auth = () => {
 
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
-  }, [])
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/',
+      });
+
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -50,7 +84,7 @@ const Auth = () => {
                 type="password"
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === 'login' ? 'Login' : 'Sign Up'}
             </button>
             <p className="text-neutral-500 mt-12">
